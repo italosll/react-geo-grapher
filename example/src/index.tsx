cb8a5f7f04e9b1ae02138ab4cf827a10a8b3e832
italosll <italomsilva.if@gmail.com>
2021-10-26 20:47:10 -0300

    💄 Update example project

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -8,7 +8,12 @@ import goias from './goias.json'
 
 const App = () => (
 
-  <BarMap style={{ background: '#345' }} geojson={goias} limits={[25, 20, 55]} colors={['#0f0', '#f00', '#000']} />
+  <BarMap
+    style={{ width: '1200px', background: '#ffffff' }}
+    geojson={goias}
+    limits={[40, 30, 30]}
+    colors={['#0077ff', '#ff7171', '#78ff73']}
+  />
 )
 
 ReactDOM.render(<App />, document.getElementById('root'))
