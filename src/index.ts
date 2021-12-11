67259137095c5677cd094f2043a3f8cea2a5e514
italosll <italomsilva.if@gmail.com>
2021-12-11 19:10:41 -0300

    ✏️ Update export components

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export { BarMap, BarMapProps } from '../src/Maps/BarMap'
+export { CircMap, CircMapProps } from '../src/Maps/CircMap'
+export { LineMap, LineMapProps } from '../src/Maps/LineMap'
+export { SquaredMap, SquaredMapProps } from '../src/Maps/SquaredMap'
